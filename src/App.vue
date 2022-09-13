ecd030a4f101c81905829ccdc497ebc19421f20d
Walid Wazini <wazini25@gmail.com>
2022-09-13 13:43:31 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="bg-slate-700 w-full h-[100vh]">
+    <NavBar />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    NavBar,
   },
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav
+    class="
+      bg-[#242481]
+      w-full
+      flex
+      items-center
+      justify-between
+      text-white
+      p-4
+      mx-auto
+    "
+  >
+    <a
+      class="
+        inline-flex
+        items-center
+        justify-center
+        w-10
+        h-10
+        bg-gray-100
+        rounded-lg
+      "
+      href="/"
+    >
+      👋
+    </a>
+    <h1 class="text-xl font-bold">V Learning Hub</h1>
+
+    <ul class="flex items-center text-sm font-medium space-x-2">
+      <li class="hidden lg:block">
+        <a class="px-3 py-2 rounded-lg" href="/"> Home </a>
+      </li>
+
+      <li><a class="px-3 py-2 rounded-lg" href=""> Projects </a></li>
+
+      <li>
+        <a
+          class="inline-flex items-center px-3 py-2 rounded-lg"
+          href=""
+          target="_blank"
+        >
+          External
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            class="ml-1.5 w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            ></path>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
